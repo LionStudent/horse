9edb99dd246e51c441be8d17242c143ce3761b47
horsestudent <horse.student@tutanota.com>
2020-05-21 21:29:23 +0000

    adds and upgrades

--- microservice.py
+++ microservice.py
@@ -6,7 +6,8 @@ from flask import jsonify
 
 
 import store
-
+import covid19
+import json
 
 app = Flask(__name__)
 
@@ -56,3 +57,20 @@ def getPosts():
     posts = store.searchTextData(name)
 
     return jsonify(posts)
+    @app.route('/sendSinglePost', methods=['POST'])
+
+def sendSinglePost():
+
+    response = {}
+
+    if request.method == 'POST':
+
+        options = json.loads(request.data)
+
+        filename = covid19.getImageFilename(options)
+
+        options['url'] = store.addImageData(filename)
+
+        store.addTextData(options)
+
+    return jsonify(options)
